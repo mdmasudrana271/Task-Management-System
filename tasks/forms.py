2a416202b842b2a60102ca857f8a746112438fd1
mdmasudrana271 <mr7123619@gmail.com>
2024-12-20 16:43:50 +0600

    change forms property

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,11 +7,5 @@ from . models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        exclude = ['is_completed']
-        
-
-
-class EditTaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
         fields = '__all__'
+
